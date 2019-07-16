b360300d984ed6b0f0154b7a9d735ef3ec6b8fb4
Ivan Kozachenko <ivan.kozachenko@softwerk.ee>
2019-07-16 11:46:31 +0300

    First commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public bool isCamFixed;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isCamFixed = !isCamFixed;
+        }
+
+        if (!isCamFixed)
+        {
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                cam.transform.Translate(0.0f, 0.0f, moveSpeed / 10.0f * Input.GetAxis("Vertical"), Space.World);
+            }
+
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                cam.transform.Translate(moveSpeed / 10.0f * Input.GetAxis("Horizontal"), 0.0f, 0.0f, Space.World);
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                cam.transform.Translate(0.0f, moveSpeed / 2.0f * Input.GetAxis("Mouse ScrollWheel"), 0.0f, Space.World);
+            }
+
+            if (Input.GetAxis("Mouse X") != 0)
+            {
+                cam.transform.Rotate(0.0f, rotationSpeed / 2.0f * Input.GetAxisRaw("Mouse X"), 0.0f, Space.World);
+            }
+
+            if (Input.GetAxis("Mouse Y") != 0)
+            {
+                cam.transform.Rotate(rotationSpeed / 2.0f * Input.GetAxisRaw("Mouse Y"), 0.0f, 0.0f, Space.Self);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CreateBaseBuildings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum baseBuildPrefab {HQ, Mine, AA_turret, Land_turret, Air_Hangar, Land_Hangar, Air_Factory, Land_Factory, RND_Center, Radar, Warehouse};
+public class CreateBaseBuildings : MonoBehaviour
+{
+    public baseBuildPrefab selectedPrefab;
+    public GameObject[] buildPrefabs;
+
+    public void CreateBuildingDraft(int draftNr)
+    {
+        Instantiate(buildPrefabs[0], new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        
+    }
+}
